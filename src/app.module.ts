e5935ed988715e0bdc794f0d422d4f83209f9848
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2026-04-06 23:28:17 +0200

    chore: initialize NestJS project

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config'
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true,
+    }), 
+    AuthModule, UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+  
+})
+export class AppModule {}
